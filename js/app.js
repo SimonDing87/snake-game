7b5cdcdb27f8fa7d10c2175b4ea21a9a952ca1e7
Simon Ding <simonding87@Simons-MacBook-Pro.local>
2015-12-14 13:20:25 -0800

    Implement free movement of snake with arrow keys

--- js/app.js
+++ js/app.js
@@ -2,4 +2,33 @@ $(document).ready(function() {
   $('#snake').on('click', function() {
     console.log('hey');
   });
+
+	// manual control
+	var keys = {};
+	$(document).keydown(function(e) {
+	    keys[e.keyCode] = true;
+	});
+
+	$(document).keyup(function(e) {
+	    delete keys[e.keyCode];
+	});
+	var moveSnake = function() {
+		for (var direction in keys) {
+			if (!keys.hasOwnProperty(direction)) continue;
+			if (direction == 37) { // left arrow key
+				$("#snake").animate({left: "-=5"}, 0);                
+			}
+			if (direction == 38) { // up arrow key
+				$("#snake").animate({top: "-=5"}, 0);  
+			}
+			if (direction == 39) { // right arrow key
+				$("#snake").animate({left: "+=5"}, 0);  
+			}
+			if (direction == 40) { // down arrow key
+				$("#snake").animate({top: "+=5"}, 0);  
+			}
+		}
+	}
+	setInterval(moveSnake, 20);
 });
+
