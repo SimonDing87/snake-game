b1987f8844d9b18473eefd62cd0b96b5beacb653
Simon Ding <simonding87@Simons-MacBook-Pro.local>
2015-12-14 14:49:03 -0800

    Make snake move right automatically

--- js/app.js
+++ js/app.js
@@ -3,32 +3,57 @@ $(document).ready(function() {
     console.log('hey');
   });
 
-	// manual control
-	var keys = {};
-	$(document).keydown(function(e) {
-	    keys[e.keyCode] = true;
-	});
+  // var moveSnake = function() {
+  // 	direction = 'right';
+  // 	$('#snake').animate
+  // }
 
-	$(document).keyup(function(e) {
-	    delete keys[e.keyCode];
-	});
+	// manual control
+	var $snake = $("#snake");
+	var gameSpeed = 500;
+
 	var moveSnake = function() {
-		for (var direction in keys) {
-			if (!keys.hasOwnProperty(direction)) continue;
-			if (direction == 37) { // left arrow key
-				$("#snake").animate({left: "-=5"}, 0);                
-			}
-			if (direction == 38) { // up arrow key
-				$("#snake").animate({top: "-=5"}, 0);  
-			}
-			if (direction == 39) { // right arrow key
-				$("#snake").animate({left: "+=5"}, 0);  
-			}
-			if (direction == 40) { // down arrow key
-				$("#snake").animate({top: "+=5"}, 0);  
-			}
-		}
+		$snake.css('left', '+=50');
 	}
-	setInterval(moveSnake, 20);
+	// if (direction == 37) { // left arrow key
+	// 			$snake.css('left', "-=5");                
+	// 		}
+	// 		if (direction == 38) { // up arrow key
+	// 			$snake.css('top', "-=5");  
+	// 		}
+	// 		if (direction == 39) { // right arrow key
+	// 			$snake.css('left', "+=5");  
+	// 		}
+	// 		if (direction == 40) { // down arrow key
+	// 			$snake.css('top', "+=5");  
+
+	// manual control
+	// var keys = {};
+	// var moveSnake = function() {
+	// 	$(document).keydown(function(e) {
+	// 	    keys[e.keyCode] = true;
+	// 	});
+
+	// 	$(document).keyup(function(e) {
+	// 	    delete keys[e.keyCode];
+	// 	});
+	// 	for (var direction in keys) {
+	// 		console.log(snakePosition);
+	// 		if (!keys.hasOwnProperty(direction)) continue;
+	// 		if (direction == 37) { // left arrow key
+	// 			$snake.animate({left: "-=5"}, 0);                
+	// 		}
+	// 		if (direction == 38) { // up arrow key
+	// 			$snake.animate({top: "-=5"}, 0);  
+	// 		}
+	// 		if (direction == 39) { // right arrow key
+	// 			$snake.animate({left: "+=5"}, 0);  
+	// 		}
+	// 		if (direction == 40) { // down arrow key
+	// 			$snake.animate({top: "+=5"}, 0);  
+	// 		}
+	// 	}
+	// };
+	setInterval(moveSnake, gameSpeed);
 });
 
