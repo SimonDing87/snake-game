5d266d714cfae1c4a2f8bb4a33ca500363955ae2
Simon Ding <simonding87@Simons-MacBook-Pro.local>
2015-12-14 16:04:40 -0800

    Add collision detection between snake and pellet

--- js/app.js
+++ js/app.js
@@ -5,16 +5,16 @@ $(document).ready(function() {
 
 	var $snake = $('#snake');
 	var $body = $('body');
+	var $pellet = $('.pellet');	
 	var gameSpeed = 500;
-	var direction = 39;
 	var snakeSize = parseInt($snake.css('width'));
+	var direction = 39;
 
-
-	$(document).keydown(function(e){
-		if (e.keyCode === 37 || e.keyCode === 38 || e.keyCode === 39 || e.keyCode === 40)
-		direction = e.keyCode
-	})
 	var moveSnake = function() {
+		$(document).keydown(function(e){
+			if (e.keyCode === 37 || e.keyCode === 38 || e.keyCode === 39 || e.keyCode === 40)
+			direction = e.keyCode;
+		})
 		if (direction === 37) {
 			$snake.css('left', '-=' + snakeSize);
 		}
@@ -33,12 +33,23 @@ $(document).ready(function() {
 		var pelletHTML = "<div class='pellet'></div>"
 		$body.prepend(pelletHTML);
 		$('.pellet').css({
-			top: Math.floor(Math.random() * 5) * snakeSize,
-			left: Math.floor(Math.random() * 5) * snakeSize
+			top: Math.floor(Math.random() * 8) * snakeSize,
+			left: Math.floor(Math.random() * 8) * snakeSize
 		})
 	};
+
+	var checkCollision = function(div1, div2) {
+		if (div1.css('top') === div2.css('top') && div1.css('left') === div2.css('left')) {
+			console.log('collided!');
+			return true;
+		}
+		console.log('no collision');
+		return false;
+	}
 	generatePellet();
-	setInterval(moveSnake, gameSpeed);
-	// setInterval(generatePellet, gameSpeed);
+	setInterval(function(){
+		moveSnake();
+		checkCollision($snake, $('.pellet'));
+	}, gameSpeed);
 });
 
